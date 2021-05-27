48daec310eca6abc53cc319fc3093058ca29e207
radzbydzi <>
2021-05-28 00:11:19 +0200

    first commit

---
+++ src/main/java/pl/pai/pai/model/Quiz.java
@@ -0,0 +1,68 @@
+package pl.pai.pai.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "quizes")
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String title;
+    String description;
+
+    @OneToMany(
+            mappedBy = "quiz",
+            cascade = CascadeType.ALL
+    )
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @JsonIgnore
+    List<QuizQuestion> questions = new ArrayList<>();
+
+    @OneToMany(
+            mappedBy = "userQuiz",
+            cascade = CascadeType.ALL
+    )
+    @JsonIgnore
+    List<QuizUsersAnswers> quizUsersAnswers = new ArrayList<>();
+
+    boolean randomOrder = false;
+
+
+    Long durationInSeconds;
+
+    boolean forEveryone = true;
+
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    User author;
+
+    String hashLink;
+
+    public Quiz(String title, String description, boolean randomOrder, Long durationInSeconds, boolean forEveryone, User author, String hashLink) {
+        this.title = title;
+        this.description = description;
+        this.randomOrder = randomOrder;
+        this.durationInSeconds = durationInSeconds;
+        this.forEveryone = forEveryone;
+        this.author = author;
+        this.hashLink = hashLink;
+    }
+}
